7b2e6d702aade7c92a143a462fb5a3a87a93be9a
Kazuki Shimizu <kazuki43zoo@gmail.com>
2015-06-03 09:37:51 +0900

    Update to CXF 3.1.0 & Add a default constructor in WsError

--- soap-spring-with-cxf/src/main/java/soap/jaxws/WsError.java
+++ soap-spring-with-cxf/src/main/java/soap/jaxws/WsError.java
@@ -2,9 +2,11 @@ package soap.jaxws;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class WsError {
     private String code;
     private String message;
